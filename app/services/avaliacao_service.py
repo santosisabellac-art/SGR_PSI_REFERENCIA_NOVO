bc49aaffaee86bcbffd33b6ea1e9fba4541bef02
santosisabellac-art <santosisabellac@gmail.com>
2026-08-15 19:55:27 -0300

    feat: adicionar atualizacao de avaliacao ao service

--- app/services/avaliacao_service.py
+++ app/services/avaliacao_service.py
@@ -9,17 +9,19 @@ class AvaliacaoService:
     def listar_por_aprendiz(self, aprendiz_id):
         return self.repository.listar_por_aprendiz(aprendiz_id)
 
-    def criar(
-        self,
-        aprendiz_id,
-        data,
-        instrumento,
-        responsavel,
-        sintese,
-        proximos_passos="",
-    ):
+    def criar(self, aprendiz_id, data, instrumento, responsavel, sintese, proximos_passos=""):
         return self.repository.criar(
             aprendiz_id=aprendiz_id,
+            data=data,
+            instrumento=instrumento,
+            responsavel=responsavel,
+            sintese=sintese,
+            proximos_passos=proximos_passos,
+        )
+
+    def atualizar(self, avaliacao_id, data, instrumento, responsavel, sintese, proximos_passos=""):
+        return self.repository.atualizar(
+            avaliacao_id=avaliacao_id,
             data=data,
             instrumento=instrumento,
             responsavel=responsavel,
