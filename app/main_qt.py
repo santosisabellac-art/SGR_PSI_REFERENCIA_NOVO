e4922900365b1433aaea680c076cb74ffcc9347f
santosisabellac-art <santosisabellac@gmail.com>
2026-08-15 20:23:17 -0300

    refactor: limpar inicializacao do aplicativo

--- app/main_qt.py
+++ app/main_qt.py
@@ -1,45 +1,20 @@
 import sys
 
-print("1 - Arquivo iniciado")
-
 from PySide6.QtWidgets import QApplication
-
-print("2 - QApplication importada")
 
 from app.database.database import criar_banco
 from app.ui_qt.main_window import MainWindow
 
-print("3 - MainWindow importada")
-
 
 def main():
-
-    print("4 - Entrou na função main")
-
-    # Cria automaticamente todas as tabelas do banco
     criar_banco()
 
     app = QApplication(sys.argv)
-
-    print("5 - QApplication criada")
-
     window = MainWindow()
-
-    print("6 - MainWindow criada")
-
     window.show()
 
-    print("7 - show() executado")
-
-    resultado = app.exec()
-
-    print("8 - Aplicação encerrada")
-
-    sys.exit(resultado)
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":
-
-    print("9 - Executando main")
-
     main()
